c79f9c130821af01e19d8e87b556a4aecbc804ee
¨deladoor¨ <jeffersondesenvolvedormaster@gmail.com>
2024-01-01 04:28:36 +0000

    acresentei comentarios auto-explicativos calculadora

--- Projeto calculadora simples JS/primeiro projeto.js
+++ Projeto calculadora simples JS/primeiro projeto.js
@@ -14,17 +14,22 @@ console.log (demonstrador)*/
 //console.log (demonstrar1)
 
 function append(value) {
-document.getElementById('dysplay').value += value;
+document.getElementById('dysplay').value += value;// acresenta o valor de dysplay ao dysplay
 }
 
 function calculate() {
-let expression = document.getElementById('dysplay').value;
-let result = eval(expression);
-document.getElementById('dysplay').value = result;
+let expression = document.getElementById('dysplay').value; //pega o valor em dysplay
+let result = eval(expression); //faz a conta dos itens que constam e dysplay
+document.getElementById('dysplay').value = result; // acrescenta o resultado da variavel result no dysplay
 }
 
-const tira = document.getElementById("dysplay").value
 
-function refresh () {location.reload()}
+function refresh(){
+let deixa = document.getElementById('dysplay').value;//pega valor em dysplay
+let refresh = parseInt(deixa) - parseInt(deixa); //subtrai dysplay por ele mesmo
+let final = eval(refresh) //faz a operação acima
+document.getElementById('dysplay').value = final;}// acresenta o resultado no dysplay
+
+
 
 
