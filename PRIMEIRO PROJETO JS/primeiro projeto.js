2888eee89f24cbd832aaae3c75b9e728e63ad592
¨deladoor¨ <jeffersondesenvolvedormaster@gmail.com>
2024-01-01 00:41:11 +0000

    alterações projeto calculadora

--- PRIMEIRO PROJETO JS/primeiro projeto.js
+++ PRIMEIRO PROJETO JS/primeiro projeto.js
@@ -1,1 +1,20 @@
-//vamos codar
+
+/*
+let primeirotexto = "hello word"
+
+console.log (primeirotexto)*/
+
+
+
+
+function demonstrar(){};
+   
+let demonstrador = document.getElementById("teste")
+
+
+
+console.log (demonstrador.value)
+
+
+
+
